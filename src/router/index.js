d1bb797eb12551a82f5d8193515a5b01234bdfb4
Louca <baigneres.louca@gmail.com>
2024-01-28 00:36:35 +0100

    🎨 Setup workspace

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import CallHistoryView from "../views/CallHistoryView.vue";
+import ContactsView from "../views/ContactsView.vue";
+import FavoritesView from "../views/FavoritesView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "call-history",
+            component: CallHistoryView,
+        },
+        {
+            path: "/contacts",
+            name: "contacts",
+            component: ContactsView,
+        },
+        {
+            path: "/favorites",
+            name: "favorites",
+            component: FavoritesView,
+        },
+    ],
+});
 
-export default router
+export default router;
---
+++ src/views/FavoritesView.vue
@@ -0,0 +1,3 @@
+<script></script>
+<template></template>
+<style scoped lang="scss"></style>
